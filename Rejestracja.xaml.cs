1e49dc7a71755d3fdbb520377e429f3edde196ca
mgremek <k.pawlak666@gmail.com>
2018-07-01 20:58:17 +0200

    Początek weryfikacji danych z formularza

--- Rejestracja.xaml.cs
+++ Rejestracja.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,6 +28,67 @@ namespace WPFSklep
         private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
         {
         
+        }
+
+        private void btnDodajKonto_Click(object sender, RoutedEventArgs e)
+        {
+            UkryjLabele(); //<-ukrywa info o bledach
+
+            string imie, nazwisko, email,adres,login,haslo1,haslo2;
+            bool sprawdzDane=true;//flaga do kontroli, czy wszystkie dane są wpisane
+
+            //imie
+            if (tbImie.Text == "")
+                sprawdzDane = false;
+            imie = tbImie.Text;
+
+            //nazwisko
+            if (tbNazwisko.Text == "")
+                sprawdzDane = false;
+            nazwisko = tbNazwisko.Text;
+
+            //Sprawdzanie poprawnosci adresu email
+            email = this.tbEmail.Text;
+            Regex regEmail = new Regex("^[a-z][a-z0-9_]*@[a-z0-9]*.[a-z]{2,3}$");
+            if (!regEmail.IsMatch(email))
+            {
+                this.lblErrEmail.Visibility = Visibility.Visible;
+                tbEmail.Clear();
+                sprawdzDane = false;
+            }
+
+
+            //sprawdzanie czy login juz istnieje w bazie
+
+
+            //sprawdzaniee czy haslo jest prawidlowe
+            Regex regPass = new Regex("^[a-z0-9]{6,}$");
+            haslo1 = pbHaslo1.Password;
+            if (!regPass.IsMatch(haslo1))
+            {
+                this.lblErrHaslo.Visibility = Visibility.Visible;
+                pbHaslo1.Clear();
+                sprawdzDane = false;
+            }
+            //sprawdzanie czy hasla się zgadzają
+            haslo2 = pbHaslo2.Password;
+            if(!haslo2.Equals(haslo1))
+            {
+                lblErrHasla.Visibility = Visibility.Visible;
+                pbHaslo2.Clear();
+                sprawdzDane = false;
+            }
+
+               
+
+        }
+        private void UkryjLabele()
+        {
+            this.lblErrDane.Visibility = Visibility.Hidden;
+            this.lblErrEmail.Visibility = Visibility.Hidden;
+            this.lblErrHasla.Visibility = Visibility.Hidden;
+            this.lblErrHaslo.Visibility = Visibility.Hidden;
+            this.lblErrLogin.Visibility = Visibility.Hidden;
         }
     }
 }
