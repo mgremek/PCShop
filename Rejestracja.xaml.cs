24c7021784c4e2c8444bbd0c8e2ef6d3f1563b10
mgremek <k.pawlak666@gmail.com>
2018-07-01 16:51:47 +0200

    new sign up window

---
+++ Rejestracja.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSklep
+{
+    /// <summary>
+    /// Interaction logic for Rejestracja.xaml
+    /// </summary>
+    public partial class Rejestracja : Window
+    {
+        public Rejestracja()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+        }
+    }
+}
