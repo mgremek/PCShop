1caaee71cfb35ad263756a19c0161047e76bfecd
mgremek <k.pawlak666@gmail.com>
2018-07-02 18:43:59 +0200

    Klasa do obsługi bazy danych

---
+++ Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSklep
+{
+    class Database
+    {
+        public Database()
+        {
+        }
+        public bool Connection()
+        { 
+            bool isCorrect=true;
+
+            //"Server=localhost\sqlexpress"
+            string message;
+            string connectionString = null;
+            //ZŁY CONNECTIONSTRING wywala wyjatek<--------------------------------
+            connectionString = "Server= GOUDAGRMLN\\SQLEXPRESS, Authentication=Windows Authentication, Database= Konfigurator, Integrated Security=True;";
+            SqlConnection db = new SqlConnection(connectionString);
+            SqlCommand komenda;
+            try
+            {
+                db.Open();
+                
+                db.Close();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                isCorrect = false;
+            }
+            return isCorrect;
+        }
+       
+    }
+}
