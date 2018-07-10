e3dac87bb6333e4728aecbf7841f2f827a37efc9
mgremek <k.pawlak666@gmail.com>
2018-07-10 12:55:08 +0200

    Dodanie folderów View, Model i ViewModel, przerzucenie do nich odpowiednich plików

---
+++ Model/Procesor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSklep
+{
+    class Procesor : Podzespol
+    {
+        int id,liczbaRdzeni, architektura, cache;
+        float taktowanie;
+        string typGniazda;
+        static int licznik;
+
+        public string TypGniazda
+        {
+            get
+            {
+                return typGniazda;
+            }
+
+            private set
+            {
+                typGniazda = value;
+            }
+        }
+
+        public Procesor()
+        {
+
+        }
+        public Procesor(int c, string mo, string pr, int lr, int arch, int ca, float takt,string tg) : base(c, mo, pr)
+        {
+            licznik++;
+            id = licznik;
+            liczbaRdzeni = lr;
+            architektura = arch;
+            cache = ca;
+            taktowanie = takt;
+            TypGniazda = tg;
+        }
+        public override string PokazDane()
+        {
+            return base.PokazDane()+ "\nTYP GNIAZDA: "+TypGniazda+"\nLICZBA RDZENI: " + liczbaRdzeni + "\nARCHITEKTURA: " + architektura + " bit\nCACHE: " + cache + " MB\nTAKTOWANIE: " + taktowanie + " GHz";
+        }
+        
+
+    }
+}
