bc1dc362cf415fdce7c8f66a8212b851fc888ed7
mgremek <k.pawlak666@gmail.com>
2018-07-25 21:58:02 +0200

    Do obsługi Commands

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFSklep.ViewModel
+{
+    class MainWindowViewModel
+    {
+        public ICommand ExecuteCommand { get; }
+        public MainWindowViewModel()
+        {
+            ExecuteCommand = new CommandHandler(Execute, () => true);
+        }
+        public void Execute()
+        {
+            Rejestracja rejestracja = new Rejestracja();
+            rejestracja.Show();
+        }
+    }
+}
