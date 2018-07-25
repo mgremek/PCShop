a1bfdf685a6165dc5c555fd4a1794bc75d1e2744
mgremek <k.pawlak666@gmail.com>
2018-07-25 22:00:47 +0200

    Dodanie Bindowania i DataContext

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPFSklep.ViewModel;
 
 namespace WPFSklep.View
 {
@@ -22,25 +23,13 @@ namespace WPFSklep.View
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            Konfigurator k = new Konfigurator();
-            this.Close();
-            k.Show();
-        }
+        private void button_Click(object sender, RoutedEventArgs e) { }
+        private void btnZalogujsie_Click(object sender, RoutedEventArgs e) { }
 
-        private void btnZalogujsie_Click(object sender, RoutedEventArgs e)
-        {
-            Logowanie logowanie = new Logowanie();
-            logowanie.Show();
-        }
-
-        private void btnZarejestrujsie_Click(object sender, RoutedEventArgs e)
-        {
-            Rejestracja rejestracja = new Rejestracja();
-            rejestracja.Show();
-        }
+        private void btnZarejestrujsie_Click(object sender, RoutedEventArgs e) { }
+        
     }
 }
