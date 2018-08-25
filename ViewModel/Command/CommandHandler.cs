c44f476cf9ac97144de5bd5de979eb31f2cc030c
mgremek <k.pawlak666@gmail.com>
2018-08-25 21:58:12 +0200

    Renamed and put into new folder

---
+++ ViewModel/Command/CommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFSklep.ViewModel
+{
+    class CommandHandler : ICommand
+    {
+        /// <summary>
+        /// Delegaty
+        /// </summary>
+        private Action action;
+        private Func<bool> canExecute;
+
+        public CommandHandler(Action action,Func<bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
