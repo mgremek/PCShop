b68e365a36cfdaa34f2541a5386b9e7481b63001
mgremek <k.pawlak666@gmail.com>
2018-12-11 22:18:47 +0100

    Nowa klasa ViewModelu do Okna Logowanie

---
+++ ViewModel/LogowanieViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace WPFSklep.ViewModel
+{
+    class LogowanieViewModel : INotifyPropertyChanged
+    {
+        public CommandHandler ExecuteCommand { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public Visibility lblErr { get; set; }
+        public LogowanieViewModel()
+        {
+            ExecuteCommand = new CommandHandler(Execute, () => true);
+        }
+        private void Execute()
+        {
+            bool sprawdzDane = true;
+            //polaczenie z bazą
+            
+        }
+        
+       
+    }
+}
