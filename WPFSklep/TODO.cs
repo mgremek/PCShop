e90bba55f5d8617ccafa77c1322919e8a47873d4
mgremek <k.pawlak666@gmail.com>
2019-03-19 17:54:05 +0100

    zmiany w LogowanieView

--- WPFSklep/TODO.cs
+++ WPFSklep/TODO.cs
@@ -9,30 +9,31 @@ namespace WPFSklep.ViewModel
     class TODO
     {
         //co jest do zrobienia
-        //NIE DZIALA BINDOWANIE LABELI ERROROWYCH NIE WIEM CZMEU BO JUZ DZIAŁAŁO JPRDL
-        //cala akcja w rejestracji + bindowanie labeli nie dzialajace!!!!!!!!!!!!!!!
 
+       //SZCZEGOLY PRODUKTU
         ///////////////////////////
         // dodany nowy window z detailsami produktu, zrobic zeby tam wrzucało xmla sformatowanego dla kazdego produktu, czyli:
         //      DONE dodac viewmodel dla windowsa, podbindowac text z textblocka, dodac formatowanie xmla,
-        //      DONEustawic, podbindowac przyciski na konfiguratorze do viewmodelu
+        //      DONE ustawic, podbindowac przyciski na konfiguratorze do viewmodelu
         //      DONE zbindowany jest tylko dtagrid cpu, zrobic reszte
+        ////////////////////////////////
+       ///KOSZYK
+        ///////////////////////////
         //KOSSZYK ZROBIC I PODBINDOWAC PRZYCISKU
         //JAK MIEC DOSTEP DO KOSZYKA? PRZEKAZYWAC W KONSTRUKTORZE CZY KLASA STATYCZNA?
         //-> koszyk jako tabele w bazie, przy dodawaniu do koszyka dodajesz produkty do tabeli, ustawiasz na status acive czy cos podobnego, przy usunieciu koszyka ustawiasz status na inactive
-        // dodac statusy słownikowe do tabeli Basketcode +
-
+        //      DONE dodac statusy słownikowe do tabeli Basketcode 
         /////////////////////////////
-        ///
+       ///WĄTKI
+        /////////////////////////////
         //wątki przy otwieraniu konfiguratora i logowaniu się???
         //cos nie rabotajet, nie da sie w nowym watku otworzyc formularza bo musi byc otwierany z watku STA, (DISPATCHER)
-
         // w kosntruktorze zrobic getcpu w tasku
         // asynchroniczne metody z await
-
         // Service1.cs w produktyService metoda GetProdukty() poprawić zeby pobieral nazwe subcategorii i odpowiednie kolmuny, zrobic tak dla reszty
 
-
+      ///BINDOWANIE LABELI
+        ////////////////////////
         // napisac swoj wlasny konwertere booleantovisibility z hidden i collapsed
     }
 }
